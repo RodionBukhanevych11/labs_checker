7f6f25b07e56670771326be0c52acb6ce0eb8453
Serhii Vovk <serhiivovk06@gmail.com>
2023-11-03 21:06:49 +0200

    backend/ui improvements

--- src/backend/app.py
+++ src/backend/app.py
@@ -10,7 +10,11 @@ _CONFIG = json.load(open("src/config.json"))
 
 
 class PredictionRequest(BaseModel):
+    caller: str
     url: str
+    login: str
+    password: str
+    lab: str
 
 
 @app.get("/")
@@ -20,19 +24,69 @@ def root():
 
 @app.post("/process")
 async def get_results(request: PredictionRequest):
+    caller = request.caller
     url = request.url
-    print(url)
-    if url:
-        return JSONResponse(content={
-            "status": "success",
-            "message": "Processed",
-            }, status_code=200)
-    else:
-        return JSONResponse(content={
-            "status": "unsuccess",
-            "message": "empty url"
-        }, status_code=400)
-
+    login = request.login
+    password = request.password
+    lab = request.lab
+    print(url, '\n', login, '\n', password)
+    
+    if caller == "button_examine":
+        if url:
+            return JSONResponse(content={
+                "status": "success",
+                "message": "Processed",
+                }, status_code=200)
+        else:
+            return JSONResponse(content={
+                "status": "unsuccess",
+                "message": "empty url"
+            }, status_code=400)
+    
+    if caller == "button_sign_up":
+        if login and password:
+            return JSONResponse(content={
+                "status": "success",
+                "message": "Processed",
+                }, status_code=210)
+        elif login and not password:
+            return JSONResponse(content={
+                "status": "unsuccess",
+                "message": "empty password",
+                }, status_code=410)
+        elif not login and password:
+            return JSONResponse(content={
+                "status": "unsuccess",
+                "message": "empty login",
+                }, status_code=411)
+        else:
+            return JSONResponse(content={
+                "status": "unsuccess",
+                "message": "empty login and password",
+                }, status_code=413)
+    
+    if caller == "button_sign_in":
+        if login and password:
+            return JSONResponse(content={
+                "status": "success",
+                "message": "Processed",
+                }, status_code=220)
+        elif login and not password:
+            return JSONResponse(content={
+                "status": "unsuccess",
+                "message": "empty password",
+                }, status_code=421)
+        elif not login and password:
+            return JSONResponse(content={
+                "status": "unsuccess",
+                "message": "empty login",
+                }, status_code=422)
+        else:
+            return JSONResponse(content={
+                "status": "unsuccess",
+                "message": "empty login and password",
+                }, status_code=423)
+        
 
 if __name__ == "__main__":
     uvicorn.run(app, port=_CONFIG["backend_port"])
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -5,21 +5,66 @@ import streamlit as st
 _CONFIG = json.load(open("src/config.json"))
 
 
-def send_request(backend_host: str, backend_port: str, url_message: str):
-    request_json = {"url": url_message}
+def send_request(backend_host: str, backend_port: str, caller: str, url_message: str = "", login_message: str = "", password_message: str = "", labs_value: str = ""):
+    request_json = {"url": url_message,
+                    "login": login_message,
+                    "password": password_message,
+                    "caller": caller}
     request_url = backend_host + ":" + backend_port + "/process"
     response = requests.post(url=request_url, json=request_json)
     return response
 
 
 def ui():
-    input_url_value = st.text_input('URL')
-    if input_url_value:
+    col1, col2 = st.columns(2)
+
+    list_labs = [
+        "Lab 1 Main",
+        "Lab 1 Additional",
+        "Lab 2 Main",
+        "Lab 2 Additional"
+    ]
+
+    with col1:
+        header_authorization = st.header("Authorization")
+        input_login_value = st.text_input('Login')
+        input_password_value = st.text_input('Password')
+        col1_1, col1_2 = st.columns(2)
+        
+        with col1_1:
+            button_sign_in = st.button("Sign up")
+        
+        with col1_2:
+            button_sign_up = st.button("Sign in")
+    with col2:
+        header_examination = st.header("Examination")
+        selectbox_labs = st.selectbox("Labs", list_labs)
+        input_url_value = st.text_input('URL')
+        button_examine = st.button("Examine")
+
+    if button_examine:
         response = send_request(backend_host=_CONFIG["backend_host"],
                     backend_port=str(_CONFIG["backend_port"]),
+                    caller="button_examine",
                     url_message=input_url_value)
         print(response.status_code)
+
+    if button_sign_up:
+        response = send_request(backend_host=_CONFIG["backend_host"],
+                    backend_port=str(_CONFIG["backend_port"]),
+                    caller="button_sign_up",
+                    login_message=input_login_value,
+                    password_message=input_password_value)
+        print(response.status_code)
     
+    if button_sign_in:
+        response = send_request(backend_host=_CONFIG["backend_host"],
+                    backend_port=str(_CONFIG["backend_port"]),
+                    caller="button_sign_in",
+                    login_message=input_login_value,
+                    password_message=input_password_value)
+        print(response.status_code)
 
 if __name__=="__main__":
     ui()
+

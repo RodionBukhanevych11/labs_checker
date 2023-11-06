226a9d0c2c1d8f11bd1d0a5a4d002f52fe8eeaf7
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2023-11-06 13:46:50 +0200

    remove extra logic

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -5,67 +5,41 @@ import streamlit as st
 _CONFIG = json.load(open("src/config.json"))
 
 
-def send_request(backend_host: str, backend_port: str, caller: str, url_message: str = "", login_message: str = "", password_message: str = "", lab_value: str = ""):
-    request_json = {"caller": caller,
-                    "url": url_message,
+def send_authorize_request(
+    backend_host: str,
+    backend_port: str,
+    action: str,
+    login_message: str = "",
+    password_message: str = "",
+):
+    request_json = {"action": action,
                     "login": login_message,
-                    "password": password_message,
-                    "lab": lab_value}
-    request_url = backend_host + ":" + backend_port + "/process"
+                    "password": password_message}
+    request_url = backend_host + ":" + backend_port + "/authorize"
     response = requests.post(url=request_url, json=request_json)
     return response
 
 
 def ui():
-    col1, col2 = st.columns(2)
-
-    list_labs = (
-        "Lab 1 Main",
-        "Lab 1 Additional",
-        "Lab 2 Main",
-        "Lab 2 Additional")
-
-    with col1:
-        header_authorization = st.header("Authorization")
-        input_login_value = st.text_input('Login')
-        input_password_value = st.text_input('Password')
-        col1_1, col1_2 = st.columns(2)
+    header_authorization = st.header("Authorization")
+    input_login_value = st.text_input('Login')
+    input_password_value = st.text_input('Password')
+    col1_1, col1_2 = st.columns(2)
+    with col1_1:
+        if st.button("Sign up"):
+            sign_up_response = send_authorize_request(backend_host=_CONFIG["backend_host"],
+                                            backend_port=str(_CONFIG["backend_port"]),
+                                            action="sign up",
+                                            login_message=input_login_value,
+                                            password_message=input_password_value)
+    with col1_2:
+        if st.button("Sign in"):
+            sign_up_response = send_authorize_request(backend_host=_CONFIG["backend_host"],
+                                            backend_port=str(_CONFIG["backend_port"]),
+                                            action="sign in",
+                                            login_message=input_login_value,
+                                            password_message=input_password_value)
         
-        with col1_1:
-            button_sign_in = st.button("Sign up")
-        
-        with col1_2:
-            button_sign_up = st.button("Sign in")
-    with col2:
-        header_examination = st.header("Examination")
-        selectbox_labs = st.selectbox("Labs", list_labs, index=None)
-        input_url_value = st.text_input('URL')
-        button_examine = st.button("Examine")
-        print(selectbox_labs)
-
-    if button_examine:
-        response = send_request(backend_host=_CONFIG["backend_host"],
-                    backend_port=str(_CONFIG["backend_port"]),
-                    caller="button_examine",
-                    url_message=input_url_value,
-                    lab_value=str(selectbox_labs))
-        print(response.status_code)
-
-    if button_sign_up:
-        response = send_request(backend_host=_CONFIG["backend_host"],
-                    backend_port=str(_CONFIG["backend_port"]),
-                    caller="button_sign_up",
-                    login_message=input_login_value,
-                    password_message=input_password_value)
-        print(response.status_code)
-    
-    if button_sign_in:
-        response = send_request(backend_host=_CONFIG["backend_host"],
-                    backend_port=str(_CONFIG["backend_port"]),
-                    caller="button_sign_in",
-                    login_message=input_login_value,
-                    password_message=input_password_value)
-        print(response.status_code)
 
 if __name__=="__main__":
     ui()
